982be03826b7a88a34eb721eb860606a9e1dacfb
Lucia Cristiani Valenzuela <lcristiani152@ieszaidinvergeles.org>
2022-03-04 10:15:40 +0100

    proyecto escrito

--- refactorizacion/src/refactorizacion/Refactorizacion.java
+++ refactorizacion/src/refactorizacion/Refactorizacion.java
@@ -10,12 +10,21 @@ package refactorizacion;
  * @author Diurno
  */
 public class Refactorizacion {
+   
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+// TODO code application logic here
+        int[] v = new int[5];
+        v[0] = 2;
+        v[1] = 4;
+        v[2] = 6;
+        v[3] = 8;
+        Prueba p = new Prueba();
+        p.va = 10;
+        p.mo(v);
+        p.bo(v);
+        p.in(v);
+
     }
-    
+
 }
