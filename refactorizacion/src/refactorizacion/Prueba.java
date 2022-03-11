f22a9bae52583a4d643310831f3e35c14be58be8
Lucia Cristiani Valenzuela <lcristiani152@ieszaidinvergeles.org>
2022-03-11 08:41:42 +0100

    ejercicio4

--- refactorizacion/src/refactorizacion/Prueba.java
+++ refactorizacion/src/refactorizacion/Prueba.java
@@ -5,53 +5,58 @@ package refactorizacion;
  * @author Lucía
  */
 public class Prueba {
-
+    
+  
     public int valor;
 //metodo que modifica el contenido de la posicion p de un array con el valor pasado
 
     public void modificar(int[] vector, int longitudActual) {
+        int longitud = vector.length;
         int i;
         int posicion = 1;
 //Mostramos los elementos del array
         System.out.print("v={");
-        for (i = 0; i < vector.length; i++) {
+        for (i = 0; i < longitud; i++) {
             System.out.print(vector[i] + ",");
         }
         System.out.println("}");
+        
 //Modificamos el array
-        if (posicion < vector.length) {
+        if (posicion < longitud) {
 //mostramos su contenido
             System.out.println("Elemento a modificar=" + vector[posicion]);
             vector[posicion] = this.valor;
         }
 //Mostramos los elementos del array
         System.out.print("v={");
-        for (i = 0; i < vector.length; i++) {
+        for (i = 0; i < longitud; i++) {
             System.out.print(vector[i] + ",");
         }
         System.out.println("}");
     }
 //metodo que borra el elemento de la posicion p desplazando a la izquierda todos los elementos
+    
 
     public void borrar(int[] vector, int longitudActual) {
         int posicion = 1, i;
 //mostramos su contenido
 //Mostramos los elementos del array
         System.out.print("vector = {");
-        for (i = 0; i < vector.length; i++) {
+        int longitud = vector.length;
+        for (i = 0; i < longitud; i++) {
             System.out.print(vector[i] + ",");
         }
         System.out.println("}");
 //Borramos el elemento
-        if (posicion < vector.length) {
+        if (posicion < longitud) {
             System.out.println("Elemento a borrar=" + vector[posicion]);
-            for (i = posicion; i > vector.length - 1; i--) {
+            for (i = posicion; i > longitud - 1; i--) {
                 vector[i] = vector[i + 1];
             }
         }
 //Mostramos los elementos del array
         System.out.print("vector = {");
-        for (i = 0; i < vector.length; i++) {
+        for (i = 0; i < longitud; i++) {
             System.out.print(vector[i] + ",");
         }
         System.out.println("}");
@@ -63,13 +68,14 @@ public class Prueba {
 //mostramos su contenido
 //Mostramos los elementos del array
         System.out.print("vector ={");
-        for (i = 0; i < vector.length; i++) {
+        int longitud = vector.length;
+        for (i = 0; i < longitud; i++) {
             System.out.print(vector[i] + ",");
         }
         System.out.println("}");
 //Borramos el elemento
-        if (posicion < vector.length) {
-            for (i = vector.length - 1; i < posicion; i++) {
+        if (posicion < longitud) {
+            for (i = longitud - 1; i < posicion; i++) {
                 vector[i] = vector[i - 1];
             }
             vector[posicion] = this.valor;
@@ -77,7 +83,7 @@ public class Prueba {
         }
 //Mostramos los elementos del array
         System.out.print("vector ={");
-        for (i = 0; i < vector.length; i++) {
+        for (i = 0; i < longitud; i++) {
             System.out.print(vector[i] + ",");
         }
         System.out.println("}");
